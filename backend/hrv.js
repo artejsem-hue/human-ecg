946a079e4939da0d8ccf96cdf67ef8818896f35e
artejsem-hue <artejsem@gmail.com>
2026-02-20 12:24:24 +0100

    Add computeTimeDomain function for HRV analysis

--- backend/hrv.js
+++ backend/hrv.js
@@ -1,1 +1,16 @@
+function computeTimeDomain(rr) {
+    if (rr.length < 2) return {};
 
+    const diffs = rr.slice(1).map((v,i)=>v-rr[i]);
+    const rmssd = Math.sqrt(diffs.map(d=>d*d).reduce((a,b)=>a+b)/diffs.length);
+
+    const mean = rr.reduce((a,b)=>a+b)/rr.length;
+    const sdnn = Math.sqrt(rr.map(v=>(v-mean)**2).reduce((a,b)=>a+b)/rr.length);
+
+    const nn50 = diffs.filter(d=>Math.abs(d)>50).length;
+    const pnn50 = (nn50/rr.length)*100;
+
+    return { rmssd, sdnn, pnn50 };
+}
+
+module.exports = { computeTimeDomain };
